074e4c001709183ec1ca539d6e8b9c77b07ea953
fatihmd <fatihmd@sabanciuniv.edu>
2020-06-24 14:02:56 +0300

    klt sparse optical flow imlementation comleted for two consecutive frames from video

--- src/sfm.cpp
+++ src/sfm.cpp
@@ -2,10 +2,16 @@
 #include <tools.h>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 using namespace std;
 
 //klt tracker
+void print_corners(cv::Mat & image, vector<cv::Point2f> & point_vec){
+	for (auto & element : point_vec){
+     	cv::circle(image, element, 3, 255, -1);
+     }
+}
 
 int main(void) {
 
@@ -21,14 +27,80 @@ int main(void) {
      cv::waitKey(0);
      cv::goodFeaturesToTrack(test_im, p0, 500, 0.3, 7, cv::Mat(), 7, true, 0.04);
      // Create a mask image for drawing purposes
-     cv::Mat mask = cv::Mat::zeros(test_im.size(), test_im.type());
-     cout<< p0[0];
-     int x,y;
-     for (auto & element : p0){
-     	cv::circle(test_im, element, 3, 255, -1);
-     }
+     
+     cout<< p0[0] << endl;
+
+     print_corners(test_im,p0);
      cv::imshow("test",test_im);
      cv::waitKey(0);
+
+     string video_path = "/home/administrator/slam_test/visual_slam/src/slow_traffic_small.mp4";
+     cv::VideoCapture capt;
+     capt.open(video_path);
+     if (!capt.isOpened()) {
+    	cerr << "ERROR! Unable to open video\n";
+     	return -1;
+    }
+
+    cv::Mat frame1, frame2, frame1_gray, frame2_gray;
+    capt.read(frame1);
+    for (int j = 0;j<50;j++){
+    	capt.read(frame2);
+    }
+    cv::imshow("frame1",frame1);
+    cv::waitKey(0);
+    cv::imshow("frame2",frame2);
+    cv::waitKey(0);
+
+    cvtColor(frame1, frame1_gray, cv::COLOR_BGR2GRAY);
+    cvtColor(frame2, frame2_gray, cv::COLOR_BGR2GRAY);
+
+    vector<cv::Point2f> p1;
+    vector<cv::Point2f> p2;
+    cv::goodFeaturesToTrack(frame1_gray, p1, 500, 0.3, 7, cv::Mat(), 7, true, 0.04);
+    cv::goodFeaturesToTrack(frame2_gray, p2, 500, 0.3, 7, cv::Mat(), 7, true, 0.04);
+
+    cv::Mat mask = cv::Mat::zeros(frame1.size(), frame1.type());
+    vector<cv::Scalar> colors;
+    cv::RNG rng;
+    for(int i = 0; i < 100; i++)
+    {
+        int r = rng.uniform(0, 256);
+        int g = rng.uniform(0, 256);
+        int b = rng.uniform(0, 256);
+        colors.push_back(cv::Scalar(r,g,b));
+    }
+    // calculate optical flow
+    vector<uchar> status;
+    vector<float> err;
+    cv::TermCriteria criteria = cv::TermCriteria((cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 10, 0.03);
+    cv::calcOpticalFlowPyrLK(frame1_gray, frame2_gray, p1, p2, status, err, cv::Size(15,15), 2, criteria);
+    vector<cv::Point2f> good_new;
+    for(uint i = 0; i < p1.size(); i++)
+    {
+        // Select good points
+        if(status[i] == 1) {
+            good_new.push_back(p1[i]);
+            // draw the tracks
+            cv::line(mask,p2[i], p1[i], colors[i], 2);
+            cv::circle(frame1, p2[i], 5, colors[i], -1);
+        }
+    }
+    cv::Mat img;
+    cv::add(frame1, mask, img);
+    cv::imshow("Frame", img);
+    cv::waitKey(0);
+    //int keyboard = cv::waitKey(30);
+    /*if (keyboard == 'q' || keyboard == 27)
+        break;*/
+    // Now update the previous frame and previous points
+    frame1_gray = frame1_gray.clone();
+    p1 = good_new;
+
+    /*for(int i = 0; i<100; i++){
+
+    }*/
      return(0);
+    
 
 }
